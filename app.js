48aa84904e4a2ac924db15289014d97449f8aa54
novice <seungyeon5374@gmail.com>
2024-04-01 18:10:37 +0900

    수정

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const nav = document.querySelector('.nav')
 const dropdowns = document.querySelectorAll('.dropdown')
 const dropdownMenus = document.querySelectorAll('.dropdown-menu')
 
-console.log(dropdownMenus)
-console.log(dropdowns)
+// console.log(dropdownMenus)
+// console.log(dropdowns)
 function displaySubmenu(e){
-    console.log(this.children)
+    console.log(this.children[1])
     if(this.children[1]){
         this.children[1].classList.add('show')
     }
